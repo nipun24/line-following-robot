09e229ca0810c3c9fb42ba6c54884d4ea18c0840
Nipun Haldar <nipunhaldar24@gmail.com>
2019-05-19 12:43:38 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Line-Following-Robot
+
+Embedded C code of a line following robot for ATMEGA16 microprocessor.
--- line_following_robot.c
+++ line_following_robot.c
@@ -1,23 +1,19 @@
-#include<avr/io.h>
+#include <avr/io.h>
 #define F_CPU16000000UL
-#include<util/delay.h>
+#include <util/delay.h>
 void main()
 {
   DDRA=0b11111100;
   DDRB=0b11111111;
   while(1)
   {
-	  if(PINA==0b00000011)
-	  PORTB=0b00000101;//forward
-	 else if(PINA==0b00000001)
-	  {//m1
-	  PORTB=0b0000001;//right
-	  }
-	  else if(PINA==0b00000010)
-	  {//m2
-	  PORTB=0b00000100;//left
-	  }
-	  else
-	  	   PORTB=0b00000000;//stop
+		if(PINA==0b00000011)
+	  		PORTB=0b00000101; //forward
+	 	else if(PINA==0b00000001)
+			PORTB=0b0000001; //right
+		else if(PINA==0b00000010)
+			PORTB=0b00000100; //left
+	  	else
+	  		PORTB=0b00000000; //stop
    }
 }
